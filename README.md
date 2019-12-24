a2fbd67c8f130ea75b6eb70ac617883403fe99dc
Ilya Shenbin <ilya.shenbin@gmail.com>
2019-12-24 19:25:57 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RecVAE
-The official PyTorch implementation of the paper "RecVAE: A New Variational Autoencoder for Top-N Recommendations with Implicit Feedback"
+The official PyTorch implementation of the paper "RecVAE: A New Variational Autoencoder for Top-N Recommendations with Implicit Feedback".
+
+This is a preliminary version, but sufficient to reproduce the results. Code will be refactored soon!
+
+Some sources from  [Variational autoencoders for collaborative filtering](https://github.com/dawenl/vae_cf) is partially used. Use preprocessing script from that repository before running RecVAE notebook.
---
+++ utils.py
@@ -0,0 +1,110 @@
+import numpy as np
+from scipy import sparse
+import pandas as pd
+import os
+import bottleneck as bn
+
+
+
+
+def load_train_data(csv_file, n_items, n_users, global_indexing=False):
+    tp = pd.read_csv(csv_file)
+    
+    n_users = n_users if global_indexing else tp['uid'].max() + 1
+
+    rows, cols = tp['uid'], tp['sid']
+    data = sparse.csr_matrix((np.ones_like(rows),
+                             (rows, cols)), dtype='float64',
+                             shape=(n_users, n_items))
+    return data
+
+
+def load_tr_te_data(csv_file_tr, csv_file_te, n_items, n_users, global_indexing=False):
+    tp_tr = pd.read_csv(csv_file_tr)
+    tp_te = pd.read_csv(csv_file_te)
+
+    if global_indexing:
+        start_idx = 0
+        end_idx = len(unique_uid) - 1
+    else:
+        start_idx = min(tp_tr['uid'].min(), tp_te['uid'].min())
+        end_idx = max(tp_tr['uid'].max(), tp_te['uid'].max())
+
+    rows_tr, cols_tr = tp_tr['uid'] - start_idx, tp_tr['sid']
+    rows_te, cols_te = tp_te['uid'] - start_idx, tp_te['sid']
+
+    data_tr = sparse.csr_matrix((np.ones_like(rows_tr),
+                             (rows_tr, cols_tr)), dtype='float64', shape=(end_idx - start_idx + 1, n_items))
+    data_te = sparse.csr_matrix((np.ones_like(rows_te),
+                             (rows_te, cols_te)), dtype='float64', shape=(end_idx - start_idx + 1, n_items))
+    return data_tr, data_te
+
+
+def get_data(global_indexing=False, dataset='pro_sg'):
+    DATA_DIR = 'ml-20m/'
+    pro_dir = os.path.join(DATA_DIR, dataset)
+    
+    unique_sid = list()
+    with open(os.path.join(pro_dir, 'unique_sid.txt'), 'r') as f:
+        for line in f:
+            unique_sid.append(line.strip())
+
+    unique_uid = list()
+    with open(os.path.join(pro_dir, 'unique_uid.txt'), 'r') as f:
+        for line in f:
+            unique_uid.append(line.strip())
+            
+    n_items = len(unique_sid)
+    n_users = len(unique_uid)
+    
+    train_data = load_train_data(os.path.join(pro_dir, 'train.csv'), n_items, n_users, global_indexing=global_indexing)
+
+
+    vad_data_tr, vad_data_te = load_tr_te_data(os.path.join(pro_dir, 'validation_tr.csv'),
+                                               os.path.join(pro_dir, 'validation_te.csv'),
+                                               n_items, n_users, 
+                                               global_indexing=global_indexing)
+
+    test_data_tr, test_data_te = load_tr_te_data(os.path.join(pro_dir, 'test_tr.csv'),
+                                                 os.path.join(pro_dir, 'test_te.csv'),
+                                                 n_items, n_users, 
+                                                 global_indexing=global_indexing)
+    
+    return train_data, vad_data_tr, vad_data_te, test_data_tr, test_data_te
+
+
+def NDCG_binary_at_k_batch(X_pred, heldout_batch, k=100):
+    '''
+    normalized discounted cumulative gain@k for binary relevance
+    ASSUMPTIONS: all the 0's in heldout_data indicate 0 relevance
+    '''
+    batch_users = X_pred.shape[0]
+    idx_topk_part = bn.argpartition(-X_pred, k, axis=1)
+    topk_part = X_pred[np.arange(batch_users)[:, np.newaxis],
+                       idx_topk_part[:, :k]]
+    idx_part = np.argsort(-topk_part, axis=1)
+    # X_pred[np.arange(batch_users)[:, np.newaxis], idx_topk] is the sorted
+    # topk predicted score
+    idx_topk = idx_topk_part[np.arange(batch_users)[:, np.newaxis], idx_part]
+    # build the discount template
+    tp = 1. / np.log2(np.arange(2, k + 2))
+
+    DCG = (heldout_batch[np.arange(batch_users)[:, np.newaxis],
+                         idx_topk].toarray() * tp).sum(axis=1)
+    IDCG = np.array([(tp[:min(n, k)]).sum()
+                     for n in heldout_batch.getnnz(axis=1)])
+    return DCG / IDCG
+
+
+def Recall_at_k_batch(X_pred, heldout_batch, k=100):
+    batch_users = X_pred.shape[0]
+
+    idx = bn.argpartition(-X_pred, k, axis=1)
+    X_pred_binary = np.zeros_like(X_pred, dtype=bool)
+    X_pred_binary[np.arange(batch_users)[:, np.newaxis], idx[:, :k]] = True
+
+    X_true_binary = (heldout_batch > 0).toarray()
+    tmp = (np.logical_and(X_true_binary, X_pred_binary).sum(axis=1)).astype(
+        np.float32)
+    recall = tmp / np.minimum(k, X_true_binary.sum(axis=1))
+    return recall
